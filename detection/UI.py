fe74e867304cab869fb3c3b2e46764dba422a9a6
aloisorozco <alois.orozco@gmail.com>
2024-01-21 02:29:57 -0500

    cleaned up UI

--- detection/UI.py
+++ detection/UI.py
@@ -18,6 +18,10 @@ class User_Interface:
         self.app.geometry("1920x1080+10+20")
         self.app.attributes("-fullscreen", True)
 
+        s = ttk.Style(self.app)
+        s.theme_use('winnative')
+        s.configure("TNotebook", tabposition='n')
+
         notebook = ttk.Notebook(self.app)
         notebook.pack()
 
@@ -29,33 +33,33 @@ class User_Interface:
 
         # Add content to Tab 1
         self.label_screen = tk.Label(tab_start_camera)
-        self.label_screen.grid(row=0, column=0)
-        self.btn_start_cam = tk.Button(tab_start_camera, text="Open Camera", command= lambda: self.open_camera(model, cap, zone, zone_polygon, zone_annotator, box_annotator)) 
-        self.btn_start_cam.grid(row=0, column=1)
+        self.label_screen.grid(row=0, column=0, padx=125, pady=5)
+        self.btn_start_cam = tk.Button(tab_start_camera, text="Open Camera", command= lambda: self.open_camera(model, cap, zone, zone_polygon, zone_annotator, box_annotator))
+        self.btn_start_cam.grid(row=1, column=0, padx=725, pady=5)
 
         label_conf = tk.Label(tab_settings, text="Confidence Percentage:")
-        label_conf.grid(row=0, column=0, padx=5, pady=5)
+        label_conf.grid(row=0, column=0, padx=300, pady=25)
 
         self.slider_conf = tk.Scale(tab_settings, from_=40, to=80, orient="horizontal", length=300)
-        self.slider_conf.grid(row=0, column=1, padx=5, pady=5)
+        self.slider_conf.grid(row=0, column=1, padx=50, pady=25)
 
         label_upd = tk.Label(tab_settings, text="Update Rate (sec/update):")
-        label_upd.grid(row=1, column=0, padx=5, pady=5)
+        label_upd.grid(row=1, column=0, padx=300, pady=25)
 
         self.slider_upd = tk.Scale(tab_settings, from_=3, to=20, orient="horizontal", length=300)
-        self.slider_upd.grid(row=1, column=1, padx=5, pady=5)
+        self.slider_upd.grid(row=1, column=1, padx=50, pady=25)
 
         label_msg = tk.Label(tab_settings, text="Message Per Update:")
-        label_msg.grid(row=2, column=0, padx=5, pady=5)
+        label_msg.grid(row=2, column=0, padx=300, pady=25)
 
         self.slider_msg = tk.Scale(tab_settings, from_=1, to=3, orient="horizontal", length=300)
-        self.slider_msg.grid(row=2, column=1, padx=5, pady=5)
+        self.slider_msg.grid(row=2, column=1, padx=50, pady=25)
 
         label_obj_size = tk.Label(tab_settings, text="Hazard Object Size Percentage Threshold:")
-        label_obj_size.grid(row=3, column=0, padx=5, pady=5)
+        label_obj_size.grid(row=3, column=0, padx=300, pady=25)
 
         self.slider_obj_size = tk.Scale(tab_settings, from_=60, to=90, orient="horizontal", length=300)
-        self.slider_obj_size.grid(row=3, column=1, padx=5, pady=5)
+        self.slider_obj_size.grid(row=3, column=1, padx=50, pady=25)
 
         notebook.pack(expand=True, fill="both")
         self.OBSTACLE_SET = {"door", "person", "car", "bicycle", "bus", "train", "truck", "bench", "chair", "cell phone", "bottle"}

9936dcb025e696b1e96e87e2188aef71fc377f80
aloisorozco <alois.orozco@gmail.com>
2024-01-21 00:20:37 -0500

    client & server base added

--- detection/main.py
+++ detection/main.py
@@ -1,13 +1,3 @@
-'''from ultralytics import YOLO
-#from ultralytics.yolo.v8.detect.predict import DetectionPredictor
-import cv2
-
-model = YOLO("yolov8x.pt")
-
-results = model.predict(source="0", show=True)
-
-print(results)'''
-
 import time
 
 import cv2

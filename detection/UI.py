dfca1653325845a66252d8477be1df703261cf15
Nicholas Pop <cocovert10@gmail.com>
2024-05-16 12:44:13 -0400

    Thread Pooling implemented
    -no longer creating a new thread each time we do speak_message()

--- detection/UI.py
+++ detection/UI.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageTk 
+from PIL import Image, ImageTk
 from tkinter import ttk
 from filter import Obstacle
 from ultralytics import YOLO
@@ -6,14 +6,16 @@ from annotators import Annotators
 
 import audio.tts as tts
 import threading
+import concurrent.futures
 import cv2
 import supervision as sv
 import time
 import tkinter as tk
 
+
 class User_Interface:
 
-    speech_thread = None
+    _speech_thread = None
 
     def __init__(self, args):
         self.app = tk.Tk()
@@ -37,64 +39,72 @@ class User_Interface:
         # Add content to Tab 1
         self.label_screen = tk.Label(tab_start_camera)
         self.label_screen.grid(row=0, column=0, padx=125, pady=5)
-        self.btn_start_cam = tk.Button(tab_start_camera, text="Open Camera", command= lambda: self.open_camera(args))
+        self.btn_start_cam = tk.Button(
+            tab_start_camera, text="Open Camera", command=lambda: self.open_camera(args))
         self.btn_start_cam.grid(row=1, column=0, padx=725, pady=5)
 
         label_conf = tk.Label(tab_settings, text="Confidence Percentage:")
         label_conf.grid(row=0, column=0, padx=300, pady=25)
 
-        self.slider_conf = tk.Scale(tab_settings, from_=40, to=80, orient="horizontal", length=300)
+        self.slider_conf = tk.Scale(
+            tab_settings, from_=40, to=80, orient="horizontal", length=300)
         self.slider_conf.grid(row=0, column=1, padx=50, pady=25)
 
         label_upd = tk.Label(tab_settings, text="Update Rate (sec/update):")
         label_upd.grid(row=1, column=0, padx=300, pady=25)
 
-        self.slider_upd = tk.Scale(tab_settings, from_=3, to=20, orient="horizontal", length=300)
+        self.slider_upd = tk.Scale(
+            tab_settings, from_=3, to=20, orient="horizontal", length=300)
         self.slider_upd.grid(row=1, column=1, padx=50, pady=25)
 
         label_msg = tk.Label(tab_settings, text="Message Per Update:")
         label_msg.grid(row=2, column=0, padx=300, pady=25)
 
-        self.slider_msg = tk.Scale(tab_settings, from_=1, to=3, orient="horizontal", length=300)
+        self.slider_msg = tk.Scale(
+            tab_settings, from_=1, to=3, orient="horizontal", length=300)
         self.slider_msg.grid(row=2, column=1, padx=50, pady=25)
 
-        label_obj_size = tk.Label(tab_settings, text="Hazard Object Size Percentage Threshold:")
+        label_obj_size = tk.Label(
+            tab_settings, text="Hazard Object Size Percentage Threshold:")
         label_obj_size.grid(row=3, column=0, padx=300, pady=25)
 
-        self.slider_obj_size = tk.Scale(tab_settings, from_=60, to=90, orient="horizontal", length=300)
+        self.slider_obj_size = tk.Scale(
+            tab_settings, from_=60, to=90, orient="horizontal", length=300)
         self.slider_obj_size.grid(row=3, column=1, padx=50, pady=25)
 
         notebook.pack(expand=True, fill="both")
-        self.OBSTACLE_SET = {"door", "person", "car", "bicycle", "bus", "train", "truck", "bench", "chair"}
+        self.OBSTACLE_SET = {"door", "person", "car",
+                             "bicycle", "bus", "train", "truck", "bench", "chair"}
 
         self.timed_out = 0
 
-        self.app.mainloop()
+        # Initialize the thread pool executor
+        self.executor = concurrent.futures.ThreadPoolExecutor(
+            max_workers=1)
 
+        self.app.mainloop()
 
     def get_confidence(self):
         return self.slider_conf.get()
-    
 
     def get_upd(self):
         return self.slider_upd.get()
-    
 
     def get_msg(self):
         return self.slider_msg.get()
-    
-    def speak_messages(self, obstacles):
+
+    def _speak_messages(self, obstacles):
         for obstacle in obstacles:
             if obstacle != None and time.time() > self.timed_out:
                 self.speech.generate_and_play(obstacle.__str__())
 
     def open_camera(self, args):
-    
+
         self.btn_start_cam.config(state=tk.DISABLED)
         frame_width, frame_height = args.webcam_resolution
         CONFIDENCE_THRESHOLD = 0.5
 
-        #change to 1 for webcam - if you have another device connected, otherwise leave at 0 for your default webcam
+        # change to 1 for webcam - if you have another device connected, otherwise leave at 0 for your default webcam
         # Capture vide + load model
         cap = cv2.VideoCapture(0)
         model = YOLO("yolov8n.pt")
@@ -103,7 +113,7 @@ class User_Interface:
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 
         annotators = Annotators(args.webcam_resolution)
-        
+
         while True:
 
             succ, frame = cap.read()
@@ -119,7 +129,8 @@ class User_Interface:
             ]
 
             obstacles = [
-                Obstacle(model.model.names[class_id], confidence, xyxy, annotators.zone_polygon)
+                Obstacle(model.model.names[class_id],
+                         confidence, xyxy, annotators.zone_polygon)
                 for xyxy, confidence, class_id, _
                 in detections
             ]
@@ -130,10 +141,12 @@ class User_Interface:
                 labels=labels
             )
 
-            if User_Interface.speech_thread is None or not User_Interface.speech_thread.is_alive():
-                obstacles_to_speak = [obstacle for obstacle in obstacles if obstacle is not None and time.time() > self.timed_out]
-                User_Interface.speech_thread = threading.Thread(target=self.speak_messages, args=(obstacles_to_speak,))
-                User_Interface.speech_thread.start()
+            if User_Interface._speech_thread is None or not User_Interface._speech_thread.is_alive():
+                # render only objects that are still on screen
+                obstacles_to_speak = [
+                    obstacle for obstacle in obstacles if obstacle is not None and time.time() > self.timed_out]
+                # submit task to thread pool
+                self.executor.submit(self._speak_messages, obstacles_to_speak)
 
             annotators.zone.trigger(detections=detections)
             frame = annotators.zone_annotator.annotate(scene=frame)

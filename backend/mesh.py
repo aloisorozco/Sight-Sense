0bd773f493ba5fb38c0940bf3be00fb60fd77064
Daniel Grimut <grimut.daniel@gmail.com>
2024-07-01 17:04:36 +0300

    working on EAR calc

--- backend/mesh.py
+++ backend/mesh.py
@@ -1,9 +1,24 @@
 import cv2
 import mediapipe.python.solutions.face_mesh as face_mesh
 
-def calc_movement(landmark, prev_coords):
+lEAR = None
+rEAR = None
+
+# These are the IDs of the landmark coords that we need to compute the EAR - its an associative array so DO NOT CHANGE THE ORDER
+r_eids = [33, 160, 158, 155, 153, 144]
+l_eids = [382, 384, 387, 263, 373, 380]
+
+def calc_movement(coords_dict, ids_to_get):
     # lets do blink detection - great ressource: https://peerj.com/articles/cs-943/
-    pass
+
+    filtered_coords = {}
+
+    for index, i in enumerate(ids_to_get):
+        filtered_coords[index + 1] = coords_dict[i]
+
+    #ear = (abs(filtered_coords[2][1] - filtered_coords[6][1]) + abs(filtered_coords[3][1] - filtered_coords[5][1])) // (2 * abs(filtered_coords[]))
+
+    return
 
 def draw(landmark_ids, coords, frame, cap):
 
@@ -19,6 +34,8 @@ def draw(landmark_ids, coords, frame, cap):
 
     for _, coord in coords_dict.items():
         cv2.circle(frame, coord, radius=1, color=(0, 0, 255), thickness=4)
+
+    return coords_dict
 
 def getUniqueLandmark(landmarks):
     landmark_set = set()
@@ -40,7 +57,7 @@ reye_indeces = getUniqueLandmark(reye_indeces)
 face = getUniqueLandmark(face)
 
 with mp_face_mesh.FaceMesh(
-        max_num_faces=1, 
+        max_num_faces=2, 
         refine_landmarks=True,
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as face_mesh:
@@ -64,8 +81,14 @@ with mp_face_mesh.FaceMesh(
                 
                 for face_landmarks in results.multi_face_landmarks:
                     lms = face_landmarks.landmark
-                    draw(leye_indeces, lms, frame, cap)
-                    draw(reye_indeces, lms, frame, cap)
+        
+                    raw_coords_dict = draw(leye_indeces, lms, frame, cap)
+                    #calc_movement(raw_coords_dict, l_eids)
+                    
+                    raw_coords_dict = draw(reye_indeces, lms, frame, cap)
+                    #calc_movement(raw_coords_dict, r_eids)
+
+                    # we need to calc here for the eye movement
                     draw(face, lms, frame, cap)
 
             cv2.imshow('Frame', frame)

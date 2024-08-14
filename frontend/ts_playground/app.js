4f1ee61fc83eac7855a65e0241f17c2a126126e7
Daniel Grimut <grimut.daniel@gmail.com>
2024-08-14 15:33:02 -0400

    learning type script
    learned:
    anything can be a type - we can force paramters to be specific types or specific values (literals)
    enums work as expected in Java/C++
    types are infered - good practice to only specify the type of a variable when it is not initialised - or when we choose to enforce the use some types of variables (often objects) to contain data structures such as tuples, where the individual item's types have to be specified.

--- frontend/ts_playground/app.js
+++ frontend/ts_playground/app.js
@@ -1,1 +1,20 @@
-console.log("test");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var person = {
+    arr: ["sports", 1]
+};
+var arr;
+arr = [];
+// arr.push(1)
+arr.push("str1");
+for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+    var i = arr_1[_i];
+    console.log(i.toUpperCase());
+}
+var n1 = 5;
+var n2 = 2.8;
+var tuple;
+tuple = [4, '4'];
+tuple[0] = 55;
+console.log(tuple);
--- frontend/ts_playground/app.ts
+++ frontend/ts_playground/app.ts
@@ -1,1 +1,25 @@
-console.log("test");
+function add(n1: number | string, n2: number | string) {
+
+    if (typeof n1 === 'number' && typeof n2 === 'number'){
+        return n1 + n2;
+    }
+    return n1.toString() + n2.toString()
+}
+
+var person = {
+    arr: ["sports", 1]
+};
+var arr;
+arr = [];
+// arr.push(1)
+arr.push("str1");
+for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+    var i = arr_1[_i];
+    console.log(i.toUpperCase());
+}
+var n1 = 5;
+var n2 = 2.8;
+var tuple;
+tuple = [4, '4'];
+tuple[0] = 55;
+console.log(tuple);

e48a7d5ef9894681f41dbd8e037ea8edcb32e35f
Daniel Grimut <grimut.daniel@gmail.com>
2024-08-14 19:48:17 -0400

    Cleaned up model
    I made put the CNN creation all into one file. Its not done yet, and has to be tested. Mainly, I need to figure out how to do a training method, and also see if this code works. The purpose of this commit is to set a general idea of how the model behaves, and the flow of data from layer to layer.
    
    The idea is: we initialise the CNN and a dense layer, and then piece together the CNN as well as the last Dense layer in the call() method - this allows Keras to understadn that the CNN and Dense layer are modules, but both part of the overall model.

--- backend/detection/cnn_model/siamese_cnn.py
+++ backend/detection/cnn_model/siamese_cnn.py
@@ -1,1 +1,42 @@
 #well merge the CNN with the rest of the layers here
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.layers import Dense, Conv2D, Input, MaxPool2D, Flatten
+
+class Siamese_CNN(Model):
+    
+    def __init__(self, shape, chanels) -> None:
+        super().__init__()
+        self._cnn = self._make_cnn(shape, chanels)
+        self.dense_sigmoid = Dense(1, activation='sigmoid')
+
+    
+    def _make_cnn(self, shape, chanels):
+        input = Input(shape=(shape,chanels))
+
+        c1 = Conv2D(kernel_size=10, filters=64, activation='relu')(input)
+        m1 = MaxPool2D()(c1)
+
+        c2 = Conv2D(kernel_size=7, filters=128, activation='relu')(m1)
+        m2 = MaxPool2D()(c2)
+
+        c3 = Conv2D(kernel_size=4, filters=128, activation='relu')(m2)
+        m3 = MaxPool2D()(c3)
+
+        c4 = Conv2D(kernel_size=4, filters=256, activation='relu')(m3)
+        flat1 = Flatten()(c4)
+        embedings = Dense(units=4096)(flat1)
+
+        return Model(inputs=input, outputs=embedings)
+
+
+    def _vector_diff(self, embedings_im1, embedings_im2):
+        return embedings_im1 - embedings_im2
+    
+
+    def call(self, inputs):
+        img1, img2 = inputs
+        embedings_im1 = self._cnn(img1)
+        embedings_im2 = self._cnn(img2)
+
+        embedings_diff = self._vector_diff(embedings_im1, embedings_im2)
+        return self.dense_sigmoid(embedings_diff)
